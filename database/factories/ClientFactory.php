6f56d1e13c4985e4b2a6d568517749d3c805e0e7
Filip Gjorgichkov <gjorgichkov.filip@gmail.com>
2026-09-04 15:40:31 +0200

    feat: add clients table and model

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->unique()->name(),
+        ];
+    }
+}
